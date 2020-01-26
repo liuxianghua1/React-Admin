22907d9c7d684b911ef3a083fb5cc3c6314b0e9e
liuxianghua <3374312139@qq.com>
2020-01-26 17:21:09 +0800

    Antd自定义主题🌈

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,24 @@
 /*
  * @Author: your name
  * @Date: 2020-01-26 16:40:33
- * @LastEditTime : 2020-01-26 16:58:02
+ * @LastEditTime : 2020-01-26 17:16:12
  * @LastEditors  : Please set LastEditors
  * @Description: 需要在package.json里面将react-scripts替换成react-app-rewired
  * @FilePath: /react-admin/config-overrides.js
  */
 //  从customize-cra引入一些相关的方法
-const { override, addLessLoader } = require("customize-cra");
+const { override, addLessLoader, fixBabelImports } = require("customize-cra");
+
+const modifVars = require("./lessVars");
+
 module.exports = override(
   addLessLoader({
-    javascriptEnabled: true
+    javascriptEnabled: true,
+    modifVars
+  }),
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
   })
 );
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Button } from "antd";
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                App
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+          <Button type='primary'>哈1哈哈哈哈</Button>11
+      </div>
+    );
+  }
 }
